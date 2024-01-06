5bff307afb111b4495fe31bd6e9981165813e264
Henriquestudying <kciqueks@gmail.com>
2024-01-06 00:51:32 -0300

    terminei

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react';
+
+import Todo from './components/Todo';
+import TodoForm from './components/TodoForm';
+import Search from './components/search';
+import Filter from './components/Filter';
+
 import './App.css'
+
+
 
 function App() {
   const [todos, setTodos] = useState([
@@ -23,22 +31,61 @@ function App() {
     },
   ]);
 
-  return <div className="app">
-    <h1>Lista de tarefas</h1>
-    <div className="todo-list">
-      {todos.map((todo) => (
-        <div className="todo">
-          <div className="content">
-            <p>{todo.text}</p>            
-          </div>
-          <div>
-            <button>Completar</button>
-            <button>x</button>
-          </div>
-        </div>
-      ))}
+    const [search,setSearch] = useState("");
+    const [filter, setFilter] = useState("");
+
+
+  const addTodo = (text, category) => {
+    
+    const newTodos = [
+      ...todos,
+      {
+        id: Math.floor(Math.random() * 10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (id) => {
+    const newTodos = [...todos];
+    newTodos.map((todo) =>
+      todo.id === id ? (todo.isCompleted = !todo.isCompleted) : todo
+      );
+      setTodos(newTodos);
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos];
+    const filteredTodos = newTodos.filter((todo) =>
+      todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  };
+
+  return (
+    <div className="app">
+      <h1>Lista de tarefas</h1>
+      <Search search={search} setSearch={setSearch} />
+      <Filter />
+      <div className="todo-list">
+        {todos
+        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase())
+        )
+        .map((todo) => (
+          <Todo 
+          key={todo.id}
+          todo={todo} 
+          removeTodo={removeTodo} 
+          completedTodo={completeTodo} />
+        ))}
+      </div>
+      <TodoForm addTodo={addTodo} />
     </div>
-  </div>;
+  );
 }
 
 export default App;
